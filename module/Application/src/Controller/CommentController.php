cb27b27e3bb2fa8564788891eaba9468e824b9bb
Pule Nong <103365141+pulenong@users.noreply.github.com>
2024-07-11 00:43:12 +0200

    Create CommentController.php

---
+++ module/Application/src/Controller/CommentController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Controller;
+
+class CommentController extends AbstractActionController
+{
+    public function __contruct()
+    {}
+
+    public function createAction(): Response
+    {
+      
+    }
+}
